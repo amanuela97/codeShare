c93b28c8c86620dff3e1473bf9f4c658eff742ef
amanuela97 <babyzewdie@gmail.com>
2022-11-16 11:24:21 +0200

    change backend url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,40 +1,34 @@
-import React from 'react';
-import App from './App.js';
-import { ApolloProvider as ApolloHooksProvider, InMemoryCache, ApolloClient} from '@apollo/react-hooks'
-import {setContext} from 'apollo-link-context';
-import { createUploadLink } from 'apollo-upload-client';
-/*import { WebSocketLink } from "@apollo/client/link/ws";
-
-const socketLink = new WebSocketLink({
-    uri: `ws://localhost:4000/graphql`,
-    options: {
-      reconnect: true,
-    },
-});*/
+import React from 'react'
+import App from './App.js'
+import {
+  ApolloProvider as ApolloHooksProvider,
+  InMemoryCache,
+  ApolloClient,
+} from '@apollo/react-hooks'
+import { setContext } from 'apollo-link-context'
+import { createUploadLink } from 'apollo-upload-client'
 
 const httpLink = createUploadLink({
-    uri: 'https://env-3746005.jelastic.metropolia.fi/graphql'
-});
+  uri: 'https://code-share-apollo-server.herokuapp.com/',
+})
 
 const authLink = setContext(() => {
-    const token = localStorage.getItem('jwtToken');
-    return {
-      headers: {
-        Authorization: token ? `Bearer ${token}` : ''
-      }
-    };
-});
+  const token = localStorage.getItem('jwtToken')
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    //@ts-ignore
-    cache: new InMemoryCache(),
-});
-
+  link: authLink.concat(httpLink),
+  //@ts-ignore
+  cache: new InMemoryCache(),
+})
 
 export default (
-    <ApolloHooksProvider client={client}>
-        <App/>
-    </ApolloHooksProvider>
-  
-);
+  <ApolloHooksProvider client={client}>
+    <App />
+  </ApolloHooksProvider>
+)
